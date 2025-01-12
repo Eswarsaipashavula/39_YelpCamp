592ba57e83d853c3da5a9338d067da2d820bbb06
pashaula_eswar_sai <eswar@LAPTOP-SKN5VA3D>
2025-01-12 18:58:06 +0530

    url updated

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const reviewRoutes = require('./routes/reviews');
 
 
 //const dbUrl = process.env.DB_URL
-const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp';
 mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
@@ -47,11 +47,12 @@ app.use(mongoSanitize({
     replaceWith:'_'
 }))
 
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
 const store = MongoStore.create({
     mongoUrl: dbUrl,
     touchAfter: 24 * 60 * 60,
     crypto: {
-        secret: 'thisshouldbeabettersecret!'
+        secret, 
     }
 });
 
@@ -62,7 +63,7 @@ store.on("error", function (e) {
 const sessionConfig = {
     store,
     name: 'session',
-    secret: 'thisshouldbeabettersecret!',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
